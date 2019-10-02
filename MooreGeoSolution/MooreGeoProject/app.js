d771d8dba2ba24182c582b04b93f850b17573d33
Markus||Khazzor <kuehner.markus@live.de>
2019-10-02 17:11:44 +0200

    Server set up but not tested yet --> postman get? and push? how

--- MooreGeoSolution/MooreGeoProject/app.js
+++ MooreGeoSolution/MooreGeoProject/app.js
@@ -1,6 +1,26 @@
-'use strict';
+const express = require('express');
+const lowdb = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
 
-console.log('Hello world');
+const adapter = new FileSync('data.json')
+const db = lowdb(adapter)
 
+var app = express();
 
-console.log("wow, sis works");
+app.use(express.json());
+
+app.use(express.static('public'))
+
+app.get('/api/scores', (req, res) => {
+    res.sendFile(__dirname + '/data/data.json');
+});
+
+app.post('/api/scores', function (req, res) {
+    db.get("scores")
+        .push({ "nick": req.body.nick, "score": req.body.score })
+        .write()
+
+    res.sendStatus(201);
+});
+
+app.listen(8080);
