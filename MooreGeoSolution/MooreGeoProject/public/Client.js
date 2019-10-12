fac4c3c274f1ae7a5689f4b3427a322c009dd647
MSI\harlekintiger <harlekintiger@aol.com>
2019-10-12 02:54:58 +0200

    UPD target logic

--- MooreGeoSolution/MooreGeoProject/public/Client.js
+++ MooreGeoSolution/MooreGeoProject/public/Client.js
@@ -4,27 +4,31 @@ var myNickName = "SSIO";
 var myCurrentScore = 0;
 var timerStart;
 
+var minTargetSpawns = 5;
+var maxTargetSpawns = 10;
+
+var targetSpawnChancePerFrame = 2;
+
 //GameExtends
 var gameHeight;
 var gameWidth;
 
-function spawnTarget() {
-    var target = document.createElement("div"); //creation
 
-    var targetStyleWidth = 15;
-    var targetStyleHeight = 15;
+function initialize() {
+    //Set Variables
+    var gameExtendsDiv = document.getElementById("game");
+    gameHeight = gameExtendsDiv.clientHeight;
+    gameWidth = gameExtendsDiv.clientWidth;
 
-    target.id = "commonTarget"; //STYLE over ID in css and in js
-    target.style.width = targetStyleWidth + 'px';
-    target.style.height = targetStyleHeight + 'px';
+    const { r, g, b } = hsv_to_rgb(0.5, 0.5, 0.95);
+    console.log(`random rgb: ${r}, ${g}, ${b} `);
+    console.log("tell me: " + generateCssCodeForRGBColor(hsv_to_rgb(0.5, 0.5, 0.95)));
 
-    target.addEventListener("click", onTargetHit); //clickEvent
-
-    document.getElementById("game").appendChild(target); //insertToGame
-
-    //setTargetSpawnPosition(target);
-    firstTargetMoveType(target); //SetMovement and position --> setonly movement!?!?!
+    tryToStartGame();
+    //document.getElementById("game").addEventListener("click", firstTargetMoveType);
 }
+
+
 
 //function setTargetSpawnPosition(element) {
 //    element.style.top = getRandomInt(0.0, gameHeight - 25) + 'px'; //25 --> might be the height where the target does not flow out of the gamewindow?
@@ -35,30 +39,51 @@ function plot(x, period, amplitude) {
     return Math.sin(x * period) * amplitude;
 }
 
-function generateWHY(x, rnd) {
-    return (plot(x, 0.01, 75 + rnd)
-        + plot(x, 0.02, 50 + rnd) * 0.25);
+function generateWHY(x, amplitudeOffset, periodeOffset, periodeScale) {
+    return (plot((x + periodeOffset) * periodeScale, 0.01, 75 + amplitudeOffset)
+        + plot((x + periodeOffset) * periodeScale, 0.02, 50 + amplitudeOffset) * 0.25);
 }
 
+function spawnTarget() {
 
-function firstTargetMoveType(element) {
-    var x = 0.0;
-    var rnd = Math.random() * 50.0;
+    // <create target>
+
+    var target = document.createElement("div"); //creation
+
+    var targetWidth = 30;
+    var targetHeight = 30;
+
+    target.id = "commonTarget"; //STYLE over ID in css and in js
+    target.style.width = targetWidth + 'px';
+    target.style.height = targetHeight + 'px';
+    target.style.backgroundColor = generateCssCodeForRGBColor(getRandomRGBValue());
+
+    target.addEventListener("click", onTargetHit); //clickEvent
+
+    document.getElementById("game").appendChild(target); //insertToGame
+
+    // <animate target>
+
+    var x = - targetWidth;
+    const amplitudeOffset = Math.random() * 50.0 + 60;   
+    const phaseOffset     = Math.random() * 600 - 300; // Verschiebung des Targets mitsamt Kurve vor oder zurück
+    const heightOffset    = Math.random() * 400 - 200; // Verschiebung der Kurve hoch oder runter
+    const periodeOffset   = Math.random() * 600 - 300; // Verschiebung der Höhen und Tiefen
+    const periodeScale    = (Math.random() + Math.random()) * 0.5 + 0.1; // Geschwindigkeit des hoch und runter bouncens
 
     var intervalTimeInMS = 5;
-    var timer = 0;
 
     //element.style.top = getRandomInt(0.0, gameHeight - 25) + 'px'; //25 --> might be the height where the target does not flow out of the gamewindow?
     //element.style.left = getRandomInt(0.0, gameWidth - 25) + 'px';
 
     var id = setInterval(() => {
-        if (timer >= 10000) {
+        if (parseInt(target.style.left) >= gameWidth + 10 /*delete target if it moved out of the window*/) {
+            target.parentNode.removeChild(target);
             clearInterval(id);
         } else {
-            timer += intervalTimeInMS;
             x += 0.5;
-            element.style.top = generateWHY(x, rnd) + gameHeight * 0.5 + 'px'; //war height*0.5
-            element.style.left = x + 'px';
+            target.style.top = (generateWHY(x + phaseOffset, amplitudeOffset, periodeOffset, periodeScale) + gameHeight * 0.5 + heightOffset) + 'px';
+            target.style.left = (x - (-phaseOffset)) + 'px';   // "x + phOff" -> "9 + 200" -> "9200". But "x - (-phOff)" -> "9 - (-200)" -> "209". Javascript -.-
         }
     }, intervalTimeInMS);
 }
@@ -94,15 +119,6 @@ function onTargetHit(event) {
 }
 
 
-function initialize() {
-    //Set Variables
-    var gameExtendsDiv = document.getElementById("game");
-    var gameHeight = gameExtendsDiv.clientHeight;
-    var gameWidth = gameExtendsDiv.clientWidth;
-
-    tryToStartGame();
-    //document.getElementById("game").addEventListener("click", firstTargetMoveType);
-}
 
 
 function tryToSubmitScore() {
@@ -124,7 +140,7 @@ function tryToStartGame() {
                 startGame()
             else
                 console.log("No game could be started")
-                });
+         });
 }
 
 //to post credentials? --> or a
@@ -153,18 +169,14 @@ function startGame() {
 }
 
 function spawnMultipleTargets() {
-    var amount = getRandomInt(1,10)
+    const amount = getRandomInt(minTargetSpawns, maxTargetSpawns);
+    console.log(`Spawned ${amount} targets`);
 
     for (var i = 0; i < amount; i++) {
         spawnTarget();
     }
 }
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-}
 
 function startGameSessionTimer() {
     timerStart = new Date().getTime();
@@ -176,6 +188,9 @@ function updateTimer() {
     var timerDiv = document.getElementById('timer');
     timerDiv.innerHTML = timer
 
+    if (Math.random() * 100 <= targetSpawnChancePerFrame)
+        spawnTarget();
+
     if (timer >= 60.0) { //60.0
         timerDiv.innerHTML = "Game Finished!";
         tryToSubmitScore();
@@ -183,6 +198,104 @@ function updateTimer() {
     }
     window.requestAnimationFrame(updateTimer)
 }
+
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+
+/* <Random color using HSV Color Space>  */
+
+/* <Code from https://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/ , converted from ruby> */
+
+/**
+ * Convert HSV color values to RGB color values.
+ * HSV values in [0..1[
+ * returns r, g, b  values from 0 to 255 as an object {r: r, g:g, b:b}
+ */
+function hsv_to_rgb(hue, saturation, value) {
+    const hue_asInt = parseInt(hue * 6);
+    const f = hue * 6 - hue_asInt;
+    const p = value * (1 - saturation)
+    const q = value * (1 - f * saturation)
+    const t = value * (1 - (1 - f) * saturation)
+    
+    let r = 0;
+    let g = 0;
+    let b = 0;
+    switch (hue_asInt) {
+
+        case 0: // r, g, b = v, t, p if h_asInt == 0
+            r = value;
+            g = t;
+            b = p;
+            break;
+
+        case 1:  // r, g, b = q, v, p if h_asInt == 1
+            r = q;
+            g = value;
+            b = p; 
+            break;
+
+        case 2:  // r, g, b = p, v, t if h_asInt == 2
+            r = p;
+            g = value;
+            b = t;
+            break;
+
+        case 3: // r, g, b = p, q, v if h_asInt == 3
+            r = p;
+            g = q;
+            b = value;
+            break;
+
+        case 5: // r, g, b = v, p, q if h_asInt == 5
+            r = value;
+            g = p;
+            b = q;
+            break;
+    }
+
+    r = parseInt(r * 256);
+    g = parseInt(g * 256);
+    b = parseInt(b * 256);
+
+    return { r: r, g: g, b: b};
+}
+
+/**
+ * Generates random RGB color values with similar brightness.
+ * returns r, g, b  values from 0 to 255 as an object {r: r, g:g, b:b}
+ */
+function getRandomRGBValue() {
+    const golden_ratio_conjugate = 0.618033988749895;
+    let hue = Math.random(); // use random start value
+
+    hue += golden_ratio_conjugate;
+    hue %= 1;
+    return hsv_to_rgb(hue, 0.5, 0.95);
+}
+
+/* </ code from website> */
+
+/**
+ * Input is {r, g, b}. Specifically made to work with the output of "getRandomRGBValue()" or "hsv_to_rgb()". Inputs are floored.
+ */
+function generateCssCodeForRGBColor({ r, g, b }) {
+    return `rgb(${Math.floor(r)},${Math.floor(g)},${Math.floor(b)})`;
+}
+
+/* </random color> */
+
+
+
+
+
 
 
 
